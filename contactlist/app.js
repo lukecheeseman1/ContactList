dbd10a6d50646fd859aee667a0ae0b7c75e7cf92
Cheeseman <lukecheeseman12@gmail.com>
2017-11-30 16:10:15 +0000

    no errors but postman is failing to post

--- contactlist/app.js
+++ contactlist/app.js
@@ -4,15 +4,22 @@ var mongoose = require('mongoose');
 var bodyparser = require('body-parser');
 var cors = require('cors');
 var path = require('path');
+var Promise = require('bluebird');
 
 var app = express();
+
+
 
 const route = require('./routes/route');
 
 //connect to mongoDB
-mongoose.connect('mongodb://localhost:27017/contactlist');
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://localhost:27017/contactlist', {
+    useMongoClient: true,
+});
 
 //on connection 
+mongoose.Promise = require('bluebird');
 mongoose.connection.on('connected', () => {
     console.log('Connected to database mongodb @ 27017');
 });
